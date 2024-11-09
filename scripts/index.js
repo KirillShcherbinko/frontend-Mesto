ef45461364c24144605a5a1338eae077a9b5ff8b
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2024-11-09 15:49:01 +0300

    added profile edit button

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,55 @@
 import { initialCards } from "./cards.js";
 
+// Попапы
+const profilePopup = document.querySelector(".popup_type_edit");
+const cardPopup = document.querySelector(".popup_type_new-card");
+const imagePopup = document.querySelector(".popup_type_image");
+
+// Кнопки
+const popupCloseButton = document.querySelector(".popup__close");
+const profilePopupButton = document.querySelector(".profile__edit-button");
+
+
+// Обработчики событий
+popupCloseButton.addEventListener("click", () => {
+    
+});
+profilePopupButton.addEventListener("click", openModal(profilePopup));
+
+function openModal(popup) {      
+    popup.classList.add('popup_is-opened');
+}
+
+// Находим форму в DOM
+const profileFormElement = profilePopup.querySelector(".popup__form");// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = profilePopup.querySelector(".popup__input_type_name");// Воспользуйтесь инструментом .querySelector()
+const jobInput = profilePopup.querySelector(".popup__input_type_description");// Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    const nameInputValue = nameInput.value;
+    const jobInputValue = jobInput.value;
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+    const profileTitle = document.querySelector(".profile__title");
+    const profileDescription = document.querySelector(".profile__description");
+
+    // Вставьте новые значения с помощью textContent
+    profileTitle.textContent = nameInputValue;
+    profileDescription.textContent = jobInputValue;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
 // @todo: Темплейт карточки
 
 const cardTemplate = document.querySelector("#card-template").content; 
